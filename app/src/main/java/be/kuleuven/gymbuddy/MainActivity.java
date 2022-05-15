f062affde4b9a7695f1aabcb38fc80f633764b5a
Nicolas Gutierrez <nicolas.gutierrezsuarez@student.kuleuven.be>
2022-05-15 19:01:33 +0200

    Added working menu bar

--- app/src/main/java/be/kuleuven/gymbuddy/MainActivity.java
+++ app/src/main/java/be/kuleuven/gymbuddy/MainActivity.java
@@ -1,18 +1,20 @@
 package be.kuleuven.gymbuddy;
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.navigation.ui.NavigationUI;
 
 import be.kuleuven.gymbuddy.databinding.ActivityMainPageBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainPageBinding binding;
+    private @NonNull ActivityMainPageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +22,15 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_calendar,
-                R.id.navigation_routines, R.id.navigation_exercises)
+                R.id.navigation_home, R.id.navigation_routines, R.id.navigation_exercises, R.id.navigation_calendar)
                 .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
+
 }
