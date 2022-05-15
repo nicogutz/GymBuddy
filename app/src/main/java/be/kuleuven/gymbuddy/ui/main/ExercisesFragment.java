f02ebdda331ae487926172006e0812d7fde6b9ba
Isabelle Costa <isabelle.costa@kuleuven.be>
2022-05-15 20:40:13 +0200

    calendar

--- app/src/main/java/be/kuleuven/gymbuddy/ui/main/ExercisesFragment.java
+++ app/src/main/java/be/kuleuven/gymbuddy/ui/main/ExercisesFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import be.kuleuven.gymbuddy.R;
 
@@ -48,6 +49,8 @@ public class ExercisesFragment extends Fragment {
         return fragment;
     }
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +58,7 @@ public class ExercisesFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
