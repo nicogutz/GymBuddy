5c4e6b7636bdc2cdc4b1e8cdddbd69c7b32ff15b
Isabelle Costa <isabelle.costa@kuleuven.be>
2022-05-16 04:37:55 +0200

    latest

--- app/src/main/java/be/kuleuven/gymbuddy/ui/exercise_page/ExercisePage.java
+++ app/src/main/java/be/kuleuven/gymbuddy/ui/exercise_page/ExercisePage.java
@@ -2,15 +2,9 @@ package be.kuleuven.gymbuddy.ui.exercise_page;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
 
 import be.kuleuven.gymbuddy.R;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class ExercisePage extends AppCompatActivity {
 
@@ -19,4 +13,5 @@ public class ExercisePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_page);
     }
+    //application stops here
 }
