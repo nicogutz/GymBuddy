fe6b0e806e456b6f2b494712d03c4bae8b061006
Nicolas Gutierrez <nicolas.gutierrezsuarez@student.kuleuven.be>
2022-05-17 01:22:43 +0200

    Refactor

--- app/src/main/java/be/kuleuven/gymbuddy/MainActivity.java
+++ app/src/main/java/be/kuleuven/gymbuddy/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -24,13 +25,16 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_activity_main);
+        NavController navController = navHostFragment.getNavController();
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_routines, R.id.navigation_exercises, R.id.navigation_calendar)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
--- app/src/main/java/be/kuleuven/gymbuddy/data/local/access/PublicExercisesDAO.java
+++ app/src/main/java/be/kuleuven/gymbuddy/data/local/access/PublicExercisesDAO.java
@@ -4,23 +4,7 @@ import androidx.room.*;
 
 import java.util.List;
 
-import be.kuleuven.gymbuddy.data.database.entities.User;
 
 @Dao
 public interface PublicExercisesDAO {
-    @Query("SELECT * FROM user")
-    List<User> getAll();
-
-    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-    List<User> loadAllByIds(int[] userIds);
-
-    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    User findByName(String first, String last);
-
-    @Insert
-    void insertAll(User... users);
-
-    @Delete
-    void delete(User user);
 }
