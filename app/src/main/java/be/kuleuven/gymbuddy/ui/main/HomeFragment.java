0500d6fdd407557d6c59da59e6183685ec5010df
Isabelle Costa <isabelle.costa@kuleuven.be>
2022-05-21 09:42:35 +0200

    other graph bugged

--- app/src/main/java/be/kuleuven/gymbuddy/ui/main/HomeFragment.java
+++ app/src/main/java/be/kuleuven/gymbuddy/ui/main/HomeFragment.java
@@ -1,11 +1,25 @@
 package be.kuleuven.gymbuddy.ui.main;
 
+import android.graphics.DashPathEffect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.CatmullRomInterpolator;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYGraphWidget;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.util.Arrays;
 
 import be.kuleuven.gymbuddy.R;
 
@@ -25,9 +39,11 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    //graph
+    //graph jjoe64
     //private LineGraphSeries<DataPoint> series1;
 
+    //other graph
+    private XYPlot plot;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -61,6 +77,63 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        //for other plot:
+        plot = (XYPlot) getActivity().findViewById(R.id.plot);
+
+        // create a couple arrays of y-values to plot:
+        final Number[] domainLabels = {1, 2, 3, 6, 7, 8, 9, 10, 13, 14};
+        Number[] series1Numbers = {1, 4, 2, 8, 4, 16, 8, 32, 16, 64};
+        Number[] series2Numbers = {5, 2, 10, 5, 20, 10, 40, 20, 80, 40};
+
+        // turn the above arrays into XYSeries':
+        // (Y_VALS_ONLY means use the element index as the x value)
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1");
+        XYSeries series2 = new SimpleXYSeries(
+                Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+
+        // create formatters to use for drawing a series using LineAndPointRenderer
+        // and configure them from xml:
+        LineAndPointFormatter series1Format =
+                new LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels);
+
+        LineAndPointFormatter series2Format =
+                new LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels_2);
+
+        // add an "dash" effect to the series2 line:
+        series2Format.getLinePaint().setPathEffect(new DashPathEffect(new float[] {
+
+                // always use DP when specifying pixel sizes, to keep things consistent across devices:
+                PixelUtils.dpToPix(20),
+                PixelUtils.dpToPix(15)}, 0));
+
+        // just for fun, add some smoothing to the lines:
+        // see: http://androidplot.com/smooth-curves-and-androidplot/
+        series1Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+        series2Format.setInterpolationParams(
+                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+        // add a new series' to the xyplot:
+        plot.addSeries(series1, series1Format);
+        plot.addSeries(series2, series2Format);
+
+        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                int i = Math.round(((Number) obj).floatValue());
+                return toAppendTo.append(domainLabels[i]);
+            }
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+            }
+        });
+    }
+
+
+        //for jjoe64 graph:
 //        double x,y;
 //        x = 0;
 //
@@ -74,7 +147,7 @@ public class HomeFragment extends Fragment {
 //            series1.appendData(new DataPoint(x,y), true, 100);
 //        }
 //        graph.addSeries(series1);
-    }
+    } //class or interface expected here?
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
