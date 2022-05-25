d96e2afd0d08fd7523da0f3bce2bcae4eb17125b
Nicolas Gutierrez <nicolas.gutierrezsuarez@student.kuleuven.be>
2022-05-25 03:18:31 +0200

    Popuuup

--- app/src/main/java/be/kuleuven/gymbuddy/ui/main/RoutinesFragmentAdapter.java
+++ app/src/main/java/be/kuleuven/gymbuddy/ui/main/RoutinesFragmentAdapter.java
@@ -1,20 +1,22 @@
 package be.kuleuven.gymbuddy.ui.main;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.text.InputType;
-import android.util.Log;
-import android.view.InputDevice;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 import be.kuleuven.gymbuddy.R;
 import be.kuleuven.gymbuddy.data.local.entities.SavedRoutine;
@@ -98,25 +100,56 @@ public class RoutinesFragmentAdapter extends BaseExpandableListAdapter {
                     Context.LAYOUT_INFLATER_SERVICE);
             convertView = layoutInflater.inflate(R.layout.routine_subgroup, null);
 
-            TableLayout tableLayout = (TableLayout) convertView.findViewById(R.id.routineExerciseListTable);
+            TableLayout tableLayout = (TableLayout) convertView.findViewById(
+                    R.id.routineExerciseListTable);
 
             ArrayList<String> savedExercises = routineList.get(groupPosition).savedExercises;
 
-            for(String name: savedExercises){
+            for (String name : savedExercises) {
                 TableRow tableRow = new TableRow(context);
-                tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                tableRow.setLayoutParams(
+                        new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+                                TableRow.LayoutParams.WRAP_CONTENT));
 
                 TextView nameTextView = new TextView(context);
                 nameTextView.setText(name);
-                nameTextView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                nameTextView.setLayoutParams(
+                        new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+                                TableRow.LayoutParams.WRAP_CONTENT));
                 tableRow.addView(nameTextView);
 
                 makeEditText(name, tableRow);
                 makeEditText(name, tableRow);
                 makeEditText(name, tableRow);
 
-                tableLayout.addView(tableRow, new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+                tableLayout.addView(tableRow,
+                        new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
+                                TableLayout.LayoutParams.WRAP_CONTENT));
             }
+
+            Button recordButton = (Button) convertView.findViewById(R.id.record_button);
+            Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(context, (l, y, m, d) -> {
+                calendar.set(y, m, d);
+            },
+                    calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH));
+
+            recordButton.setOnClickListener(v -> {
+                for (int i = 1; i < tableLayout.getChildCount(); i++) {
+                    TableRow row = (TableRow) tableLayout.getChildAt(i);
+                    String name = (String) ((TextView) row.getChildAt(0)).getText().toString();
+                    // Who says Java is verbose
+                    int sets = Integer.parseInt(
+                            (String) ((TextView) row.getChildAt(1)).getText().toString());
+                    int reps = Integer.parseInt(
+                            (String) ((TextView) row.getChildAt(2)).getText().toString());
+                    int weight = Integer.parseInt(
+                            (String) ((TextView) row.getChildAt(3)).getText().toString());
+                }
+                datePickerDialog.show();
+            });
 
         }
 
@@ -125,8 +158,10 @@ public class RoutinesFragmentAdapter extends BaseExpandableListAdapter {
 
     private void makeEditText(String name, TableRow tableRow) {
         EditText editText = new EditText(context);
+        editText.setText("0");
         editText.setInputType(InputType.TYPE_CLASS_NUMBER);
-        editText.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        editText.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT));
         tableRow.addView(editText);
     }
 
